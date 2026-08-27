98a2211f9af25b0c913b990b24d32b5674d4b7d8
polar-n0de <247172282+polar-n0de@users.noreply.github.com>
2026-08-27 18:46:16 +0200

    fix: add required_providers block to ha-node module

--- terraform/modules/ha-node/main.tf
+++ terraform/modules/ha-node/main.tf
@@ -1,14 +1,58 @@
 terraform {
   required_providers {
     proxmox = {
-      source  = "bpg/proxmox"
-      version = "~> 0.66"
+      source = "bpg/proxmox"
     }
   }
 }
 
-provider "proxmox" {
-  endpoint  = var.pm_api_url
-  api_token = var.pm_api_token
-  insecure  = true
+resource "proxmox_virtual_environment_vm" "node" {
+  name        = var.name
+  vm_id       = var.vm_id
+  node_name   = var.target_node
+  description = "Managed by Terraform - HA Guacamole/Galera stack"
+  tags        = sort(var.tags)
+
+  clone {
+    vm_id = var.template_vm_id
+    full  = true
+  }
+
+  agent {
+    enabled = true
+  }
+
+  cpu {
+    cores = var.cores
+  }
+
+  memory {
+    dedicated = var.memory_mb
+  }
+
+  disk {
+    datastore_id = var.datastore_id
+    interface    = "scsi0"
+    size         = var.disk_size_gb
+  }
+
+  network_device {
+    bridge = "vmbr0"
+    model  = "virtio"
+  }
+
+  initialization {
+    ip_config {
+      ipv4 {
+        address = var.ipv4_address
+        gateway = var.ipv4_gateway
+      }
+    }
+    user_account {
+      username = var.ci_username
+      keys     = var.ssh_public_keys
+    }
+  }
+
+  started = true
 }
